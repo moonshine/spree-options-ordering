d7f84e9084ea1fc7e58a517ba49684d71770fb91
Etienne van Tonder <etiennevt@gmail.com>
2010-06-17 18:48:15 +1000

    Fix problem with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 # Add action site_reorder_values to reorder the option values for an option type
-admin.resources :products, :member => {:clone => :get}, :has_many => [:product_properties, :images] do |product|
-  product.resources :option_types, :member => { :select => :get, :remove => :get}, :collection => {:available => :get, :selected => :get, :site_reorder_values => :post}
+map.namespace :admin do |admin|
+  admin.resources :products, :member => {:clone => :get}, :has_many => [:product_properties, :images] do |product|
+    product.resources :option_types, :member => { :select => :get, :remove => :get}, :collection => {:available => :get, :selected => :get, :site_reorder_values => :post}
+  end
 end
