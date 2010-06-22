6eb60d85ca393c3e8d7470f7f0e4348f443c1743
Etienne van Tonder <etiennevt@gmail.com>
2010-06-22 16:23:48 +1000

    Do not make overriden methods private

--- options_ordering_hooks.rb
+++ options_ordering_hooks.rb
@@ -1,6 +1,8 @@
 class OptionsOrderingHooks < Spree::ThemeSupport::HookListener
 
-  insert_after :admin_inside_head, 'admin/shared/scripts'
+  insert_after :admin_inside_head do
+    "<%= javascript_include_tag 'jquery-ui-1.7.3.custom.min' %>"
+  end
   insert_after :admin_option_type_edit_form, 'admin/option_types/options_ordering_form'
 
 end
